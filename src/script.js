7bb639db996cfa2793a10700cda1666176d024b3
Antonio C <antonio.cancarevic@live.com>
2022-04-04 12:18:44 +0200

    fun incoming, hidden song ready

--- src/script.js
+++ src/script.js
@@ -1,47 +1,86 @@
-var playBtn = document.getElementById('play'),
-    resetBtn = document.getElementById('reset'),
-    drum_loop = document.getElementById('drum_loop'),
-    piano_loop = document.getElementById('piano_loop'),
-    test_button = document.getElementById('test_button'),
-    audios = document.querySelectorAll('audio');
+let audios = document.querySelectorAll('audio');
 console.log(audios);
 
-let playing = false;
+let lead_1_img = document.getElementById('lead_1'),
+    lead_2_img = document.getElementById('lead_2'),
+    ambience_img = document.getElementById('ambience'),
+    drums_img = document.getElementById('drums'),
+    clarinet_img = document.getElementById('clarinet'),
+    voice_img = document.getElementById('voice'),
 
-playBtn.addEventListener('click', function() {
-    drum_loop.loop = true;
-    if(!playing) {
-        drum_loop.play();
-        playing = true;
-    } else {
-        drum_loop.pause();
-        playing = false;
-    }
+    lead_1_audio = document.getElementById('lead_1_loop'),
+    lead_2_audio = document.getElementById('lead_2_loop'),
+    ambience_audio = document.getElementById('ambience_loop'),
+    drums_audio = document.getElementById('drums_loop'),
+    clarinet_audio = document.getElementById('clarinet_loop'),
+    voice_audio = document.getElementById('voice_loop')
+
+    init_button = document.getElementById('initiate')
+
+    /* for hover test */
+    test_button = document.getElementById('test')
+    piano_hover = document.getElementById('piano_loop')
+
+
+init_button.addEventListener('click', function(){
+    lead_1_audio.volume = 0;
+    lead_2_audio.volume = 0;
+    ambience_audio.volume = 0;
+    drums_audio.volume = 0;
+    clarinet_audio.volume = 0;
+    voice_audio.volume = 0;
+
+    lead_1_audio.loop = true;
+    lead_2_audio.loop = true;
+    ambience_audio.loop = true;
+    drums_audio.loop = true;
+    clarinet_audio.loop = true;
+    voice_audio.loop = true;
+
+    lead_1_audio.play();
+    lead_2_audio.play();
+    ambience_audio.play();
+    drums_audio.play();
+    clarinet_audio.play();
+    voice_audio.play();
+})
+
+lead_1_img.addEventListener('click', function() {
+    lead_1_audio.volume = .3;
 }, false);
 
-resetBtn.addEventListener('click', function() {
-    piano_loop.loop = true;
-    if(!playing) {
-        piano_loop.play();
-        playing = true;
-    } else {
-        piano_loop.pause();
-        playing = false;
-    }
+lead_2_img.addEventListener('click', function() {
+    lead_2_audio.volume = .3;
+
+}, false);
+
+ambience_img.addEventListener('click', function() {
+    ambience_audio.volume = .3;
+}, false);
+
+drums_img.addEventListener('click', function() {
+    drums_audio.volume = 1;
+}, false);
+
+voice_img.addEventListener('click', function() {
+    voice_audio.volume = .5;
+}, false);
+
+clarinet_img.addEventListener('click', function() {
+    clarinet_audio.volume = .3;
+}, false);
+
+
+/* HOVER EFFECT */
+
+test_button.addEventListener('mouseover', function() {
+    piano_hover.volume = 1;
+    piano_hover.play();
 }, false);
 
 /*
-resetBtn.addEventListener('mouseover', function() {
-    piano_loop.play();
-}, false);
-
-resetBtn.addEventListener('mouseleave', function() {
-    piano_loop.pause();
-    piano_loop.currentTime = 0;
+test_button.addEventListener('mouseleave', function() {
+    fadeOut(piano_hover);
+    piano_hover.currentTime = 0;
 }, false);
 */
-
-test_button.addEventListener('click', function() {
-    div.transform.rotate(90);
-}, false);
-
